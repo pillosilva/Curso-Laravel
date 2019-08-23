7fde3fa28016a2c1a05c7343e171b1400e03b783
proyectlaravel <elpidiosilvaabundis@gmail.com>
2019-08-23 13:54:06 -0500

    clase 15 mensaje desde el controlador

---
+++ resources/views/admin/menu/crear.blade.php
@@ -0,0 +1,30 @@
+@extends("theme.$theme.layout")
+@section('titulo')
+Sistemas Menús
+@endsection
+
+@section('contenido')
+<div class="row">
+        <div class="col-lg-12">
+            @include('includes.form-error')
+            @include('includes.mensaje')
+            <div class="box box-danger">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Crear Menú</h3>
+                </div>
+            <form action="{{route('guardar_menu')}}" id="form-general" class="form-horizontal" method="POST">
+                @csrf
+                    <div clas="box-body">
+                        @include('admin.menu.form')
+                    </div>
+                    <div class="box-footer">
+                        <div class="col-lg-3"></div>
+                            <div class="col-lg-6">
+                                 @include('includes.boton-form-crear')
+                        </div>  
+                    </div>
+                </form>
+            </div>
+        </div>
+</div>        
+@endsection
